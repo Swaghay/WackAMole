b26c5b5fa2d91135aa7dcae2cc9dd97738b9518d
Abhay Upadhyaya <64004046@ep-student.org>
2020-09-14 13:40:28 -0500

    Initial Commit

---
+++ wackAmole/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wackAmole
+//
+//  Created by 64004046 on 9/14/20.
+//  Copyright © 2020 64004046. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
